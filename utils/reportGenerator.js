66f3a267b9db985ea075641ec4146d21d189105d
downtothemeyer <jmeyer1220@gmail.com>
2024-08-02 14:55:21 -0500

    Update reportGenerator.js

--- utils/reportGenerator.js
+++ utils/reportGenerator.js
@@ -2,19 +2,23 @@
 import { analyzePlatform } from './platform';
 import { crawlWebsite } from './crawl';
 import { measurePerformance } from './performance';
+import performChurchSpecificAnalysis from '../../utils/churchSpecificAnalysis';
 
 export async function generateReport(url) {
   //const scrapedData = await scrapeWebsite(url);
   const platformData = await analyzePlatform(url);
   const crawlData = await crawlWebsite(url);
   const performanceData = await measurePerformance(url);
+  const churchSpecificResults = await performChurchSpecificAnalysis(url);
+
 
   return {
     url,
     //scrapedData,
-    platformData,
     crawlData,
+    platformData,
     performanceData,
+    churchSpecificResults,
     generatedAt: new Date().toISOString(),
   };
 }
